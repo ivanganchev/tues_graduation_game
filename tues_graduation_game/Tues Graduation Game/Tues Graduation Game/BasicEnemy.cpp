50ff22a661c25decdee8ac8d8a04fc1728afa1dd
somalieca <bigman_cr@abv.bg>
2017-04-29 23:40:22 +0300

    Collision testing v.02

---
+++ tues_graduation_game/Tues Graduation Game/Tues Graduation Game/BasicEnemy.cpp
@@ -0,0 +1,64 @@
+#include <SDL.h>
+#include "Headers/GameObject.h"
+#include "Headers/BasicEnemy.h"
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include "Headers/EnemyAmmo.h"
+#include "Headers/EnemyAmmoGen.h"
+
+BasicEnemy::BasicEnemy(SDL_Surface* screenSurface, SDL_Window* window, vector <GameObject*> *gameObjects) : GameObject("pictures/basicenemyplanev1.png", screenSurface, window)
+{
+	ammoGen_ = new EnemyAmmoGen(screenSurface, window, gameObjects);
+	gameObjects_ = gameObjects;
+	moveFlag_ = true;
+	srand(time(NULL));
+	limit = rand() % 100 + 1;
+	speedY = rand() % 20 + 2;
+	speedX = rand() % 20 + 2;
+	if (speedX % 2 == 0)
+	{
+		speedX = -speedX;
+	}
+}
+
+bool BasicEnemy::isHit(int xCord, int yCord) 
+{
+
+	if(xCord >= gameObjects_[i] && xCord <= (x_ - 11))
+
+	return 1;
+}
+
+void BasicEnemy::move()
+{
+
+		int h, w;
+		
+		if(y_ < limit)
+		{
+			y_ += speedY;
+			
+			
+		}
+		else
+		{
+			x_ += speedX;
+
+		}
+		GameObject *tempPointer = ammoGen_->get_object();
+		
+		ammoGen_->add(1, 3);
+		if (tempPointer != ammoGen_->get_object()) {
+			ammoGen_->get_object()->set_x(x_ + 65);
+			ammoGen_->get_object()->set_y(y_ + 109);	
+		}
+
+		SDL_GetWindowSize(window_, &w, &h);
+		if (x_ > w || x_ < -120)
+		{
+			isVisible_ = false;
+		}
+
+
+}
---
+++ tues_graduation_game/Tues Graduation Game/Tues Graduation Game/Headers/Background.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "GameObject.h"
+#include <SDL.h>
+#include <SDL_image.h>
+
+
+class Background: public GameObject
+{
+
+public:
+	Background(SDL_Surface* screenSurface, SDL_Window* window);
+	
+
+
+	void move();
+};
